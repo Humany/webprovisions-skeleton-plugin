5abf715be5d83a4d1efd037e32d1916e4e09f8aa
Bratn <andreas.brantmo@humany.com>
2018-03-13 08:33:02 +0100

    Remove commented code.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ if (!humany) {
   console.error('No Humany installation is available on the page.');
 }
 
-// if (!humany.webprovisions) {
-//   console.error('The current Humany installation doesn´t have the Webprovisions runtime enabled.');
-// }
-
 humany.configure(settings.implementationName, (config) => {
-
   // Register the plugin in the types registry for the current environment:
   config.types.register('@my/skeleton-plugin', SkeletonPlugin);
   // Enable the plugin for the specified widget:
